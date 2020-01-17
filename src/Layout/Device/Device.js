295b9db527017bb34ee9992fcf141e25e3c60373
nosamendev <nosamendev@gmail.com>
2020-01-17 11:54:00 +0200

    two levels of battery warning

--- src/Layout/Device/Device.js
+++ src/Layout/Device/Device.js
@@ -1,16 +1,23 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import './Device.css'; 
 
 const Device = (props) => {
-    useEffect(() => {
-        //console.log(props);
-        
-    }, [])
+
+    let className = "";
+    if (props.battery < 50) {
+        if (props.battery > 30) {
+            className = "warn1"
+        }
+        else {
+            className = "warn2";
+        }
+    }
+
     return (
         <tr>
             <td>{props.id}</td>
             <td>{props.fCntUp}</td>
-            <td className={(props.battery < 50) ? "warn" : null}>{props.battery}</td>
+            <td className={className}>{props.battery}</td>
             <td>{props.lat}</td>
             <td>{props.lng}</td>
             <td>{props.barrierId}</td>
